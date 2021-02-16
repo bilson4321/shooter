796d7e6ccedeb06c34bf700cdfe62e3a5d1f6b35
bilson4321 <bilsonc47@gmail.com>
2021-02-16 13:32:32 +0545

    added main menu

---
+++ Assets/Scripts/Main Menu/MainMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onPlayButtonClick()
+    {
+        Debug.Log("Play Button pressed");
+        SceneManager.LoadScene(1);
+    }
+}

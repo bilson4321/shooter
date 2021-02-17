ce98f7139fa8b9fa387d95321f064e5fdd702a7e
bilson4321 <bilsonc47@gmail.com>
2021-02-17 12:05:29 +0545

    added shooting mechanism

---
+++ Assets/Scripts/GamePlay/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
